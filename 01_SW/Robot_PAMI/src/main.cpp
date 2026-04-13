161d9a215e829e01951148a17a1a4f5c411e3f58
Clement <56961627+Verboom30@users.noreply.github.com>
2026-04-13 23:00:08 +0200

    PAMI : Laser sensor

--- 01_SW/Robot_PAMI/src/main.cpp
+++ 01_SW/Robot_PAMI/src/main.cpp
@@ -1,200 +1,250 @@
 #include "mbed.h"
 #include <inttypes.h>
 #include <cmath>
+
 #include "pinout.hpp"
 #include "main_pck.hpp"
 #include "Stepper.hpp"
 #include "UART_TMC.hpp"
 #include "Differentiel.hpp"
 #include "VL53L0X_mbed.hpp"
-//#define DEBUG
-#define NORMALSPEED  1.0f
-#define SLOWSPEED    0.4f
 
-//***********************************/************************************
-//                              UART_TMC                                //
-//***********************************/************************************
+//#define DEBUG
+
+#define NORMALSPEED 1.0f
+#define SLOWSPEED   0.4f
+
+//*****************************************************************************
+//                                 UART_TMC
+//*****************************************************************************
 Uart_TMC TMCSerial(TMC_UART_TX, TMC_UART_RX, R_SENSE);
 BufferedSerial pc(USBTX, USBRX, 230400);
-//***********************************/************************************
-//                                 MOVE                                 //
-//***********************************/************************************
-bool StopMove = false;
-bool Fin_de_match = false;
-bool   Couleur_Team = 0; // false bleu true jaune
+
+//*****************************************************************************
+//                                   MOVE
+//*****************************************************************************
+bool StopMove      = false;
+bool Fin_de_match  = false;
+bool Couleur_Team  = false;   // false = bleu / true = jaune
+
 Stepper StepperG(STEP_G, DIR_G, false);
 Stepper StepperD(STEP_D, DIR_D, true);
+
 differentiel RobotDiff(&StepperG, &StepperD, &StopMove);
-//***********************************/************************************
-//                                 SWITCH                               //
-//***********************************/************************************
+
+//*****************************************************************************
+//                                  SWITCH
+//*****************************************************************************
 DigitalIn SW_team(SW_TEAM);
 DigitalIn SW_bau(SW_BAU);
 DigitalIn SW_Tirette(TIRETTE);
-//***********************************/************************************
-//                                 SERVO                                //
-//***********************************/************************************
+
+//*****************************************************************************
+//                                  SERVO
+//*****************************************************************************
 PwmOut Servo1(SERVO_1);
 PwmOut Servo2(SERVO_2);
-//***********************************/************************************
-//                                 LED                                  //
-//***********************************/************************************
+
+//*****************************************************************************
+//                                   LED
+//*****************************************************************************
 DigitalOut LedR(LED_R);
 DigitalOut LedG(LED_G);
 DigitalOut LedB(LED_B);
-//***********************************/************************************
-//                               LASER SENSOR                           //
-//***********************************/************************************
 
+//*****************************************************************************
+//                              LASER SENSOR
+//*****************************************************************************
 Thread sensor_thread;
+
 I2C i2c(VL53L0_I2C_SDA, VL53L0_I2C_SCL);
 DigitalOut xshut(XSHUT_1);
+
 VL53L0X_mbed* g_tof = nullptr;
 
-int state = 0;
+//*****************************************************************************
+//                                VARIABLES
+//*****************************************************************************
+int state         = 0;
+int score         = 0;
+int offset_posX   = 0;
+int offset_Alpha  = 1;
 
 volatile bool end_match = false;
+
 Timeout endMatch;
 Thread game_thread;
 
-int score = 0;
-int offset_posX = 0;
-int offset_Alpha = 1;
-
+//*****************************************************************************
+//                             SENSOR THREAD
+//*****************************************************************************
 void checkSensors()
 {
-  printf("Thread sensor start\r\n");
+    printf("Thread sensor start\r\n");
 
-  i2c.frequency(400000);
-  ThisThread::sleep_for(10ms);
-  xshut = 1;
-  ThisThread::sleep_for(50ms);
+    i2c.frequency(400000);
 
-  //while (g_tof == nullptr ) ThisThread::sleep_for(10ms);
+    ThisThread::sleep_for(10ms);
+    xshut = 1;
+    ThisThread::sleep_for(50ms);
 
-  g_tof->setTimeout(200);
+    // while (g_tof == nullptr)
+    //     ThisThread::sleep_for(10ms);
 
-  printf("Init sensor...\r\n");
-  ThisThread::sleep_for(100ms);
+    g_tof->setTimeout(200);
 
-  if (!g_tof->init()) {
-    printf("VL53L0X init failed\r\n");
-    while (true) {
-      ThisThread::sleep_for(500ms);
-    }
-  }
-
-  printf("Sensor OK\r\n");
-
-  while (true) {
-    uint16_t distance = g_tof->readRangeSingleMillimeters();
-    if (g_tof->timeoutOccurred() || distance == 65535) {
-      printf("Read error\r\n");
-    } else {
-      printf("Distance = %u mm\r\n", distance);
-    }
-
+    printf("Init sensor...\r\n");
     ThisThread::sleep_for(100ms);
-  }
+
+    if (!g_tof->init())
+    {
+        printf("VL53L0X init failed\r\n");
+
+        while (true)
+        {
+            ThisThread::sleep_for(500ms);
+        }
+    }
+
+    printf("Sensor OK\r\n");
+
+    while (true)
+    {
+        uint16_t distance = g_tof->readRangeSingleMillimeters();
+
+        if (g_tof->timeoutOccurred() || distance == 65535)
+        {
+            printf("Read error\r\n");
+        }
+        else
+        {
+            printf("Distance = %u mm\r\n", distance);
+            StopMove = (distance < 150) ? 1 : 0;
+            LedR = StopMove;
+        }
+
+        ThisThread::sleep_for(100ms);
+    }
 }
 
+//*****************************************************************************
+//                              END MATCH
+//*****************************************************************************
 void endMatchProcess()
 {
-  end_match = true;
+    end_match = true;
 }
 
+//*****************************************************************************
+//                            UTILS FUNCTIONS
+//*****************************************************************************
 float theta2pluse(int theta)
 {
-  return 500.0 + (100.0 / 9.0) * float(theta);
+    return 500.0f + (100.0f / 9.0f) * float(theta);
 }
+
 void printPosition()
 {
-  //printf pour processing
-  printf("%f;%f;%f\r\n",
-        RobotDiff.getPositionX(),
-        RobotDiff.getPositionY(),
-        RobotDiff.getAlpha());
+    // printf pour processing
+    printf("%f;%f;%f\r\n",
+           RobotDiff.getPositionX(),
+           RobotDiff.getPositionY(),
+           RobotDiff.getAlpha());
 }
 
-//--- Thread d'affichage de position ---
+//*****************************************************************************
+//                      THREAD AFFICHAGE POSITION
+//*****************************************************************************
 void routineAffichage()
 {
-  while (true)
-  {
-    printPosition();
-    ThisThread::sleep_for(25ms);
-  }
-}
-
-void main_thread(void) 
-{
-  FsmState = IDLE;
-  while (1)
-  {
-    switch (FsmState)
+    while (true)
     {
-    case IDLE:
-      RobotDiff.setPosition(0, 0, 0, Couleur_Team);
-      RobotDiff.Robotgoto(0, 1000, 90, Couleur_Team, NORMALSPEED);
-      RobotDiff.Robotgoto(1000, 1000, 180, Couleur_Team, NORMALSPEED);
-      RobotDiff.Robotgoto(1000, 0, -90, Couleur_Team, NORMALSPEED);
-      RobotDiff.Robotgoto(0, 0, 0, Couleur_Team, NORMALSPEED);
-      FsmState = END;
-      break;
-
-    case START_UP:
-   
-     
-      break;
-
-    case CAL:
-      break;
-
-    case WAIT_MATCH:
-      break;
-
-    case GAME:
-      FsmState = END;
-      break;
-    case END:
-      break;
+        printPosition();
+        ThisThread::sleep_for(25ms);
     }
-    ThisThread::sleep_for(10ms);
-  } 
 }
- 
+
+//*****************************************************************************
+//                                MAIN FSM
+//*****************************************************************************
+void main_thread(void)
+{
+    FsmState = IDLE;
+
+    while (1)
+    {
+        switch (FsmState)
+        {
+            case IDLE:
+                RobotDiff.setPosition(0, 0, 0, Couleur_Team);
+
+                RobotDiff.Robotgoto(0,    1000,   90, Couleur_Team, NORMALSPEED);
+                RobotDiff.Robotgoto(1000, 1000,  180, Couleur_Team, NORMALSPEED);
+                RobotDiff.Robotgoto(1000, 0,     -90, Couleur_Team, NORMALSPEED);
+                RobotDiff.Robotgoto(0,    0,       0, Couleur_Team, NORMALSPEED);
+
+                FsmState = END;
+                break;
+
+            case START_UP:
+                break;
+
+            case CAL:
+                break;
+
+            case WAIT_MATCH:
+                break;
+
+            case GAME:
+                FsmState = END;
+                break;
+
+            case END:
+                break;
+        }
+
+        ThisThread::sleep_for(10ms);
+    }
+}
+
+//*****************************************************************************
+//                                   MAIN
+//*****************************************************************************
 int main()
 {
-  #ifdef DEBUG
+#ifdef DEBUG
     Thread threadAffichage;
     threadAffichage.start(routineAffichage);
-  #endif;
-  printf("Nucelo ON\r\n");
-  SW_team.mode(PullUp);
-  SW_bau.mode(PullUp);
-  SW_Tirette.mode(PullUp);
-  LedR = 0;
-  LedG = 0;
-  LedB = 0;
-  TMCSerial.setup_all_stepper();
+#endif
 
-  static VL53L0X_mbed tof(i2c);   // adresse par défaut 0x29 (7 bits)
-  g_tof = &tof;
+    printf("Nucleo ON\r\n");
 
-  sensor_thread.start(checkSensors);
-  game_thread.start(main_thread);
- 
-  while (1)
-  {
-    if ((end_match or SW_bau == 1) and Fin_de_match == false)
+    SW_team.mode(PullUp);
+    SW_bau.mode(PullUp);
+    SW_Tirette.mode(PullUp);
+
+    LedR = 0;
+    LedG = 0;
+    LedB = 0;
+
+    TMCSerial.setup_all_stepper();
+
+    static VL53L0X_mbed tof(i2c);   // adresse par défaut 0x29 (7 bits)
+    g_tof = &tof;
+
+    sensor_thread.start(checkSensors);
+    game_thread.start(main_thread);
+
+    while (1)
     {
-      Fin_de_match = true;
-      //game_thread.terminate();
-      //sensor_thread.terminate();
+        if ((end_match || SW_bau == 1) && (Fin_de_match == false))
+        {
+            Fin_de_match = true;
 
+            // game_thread.terminate();
+            // sensor_thread.terminate();
+        }
+
+        ThisThread::sleep_for(1s);
     }
-    ThisThread::sleep_for(1s);
-  }
 }
-
-
