846e298e0ee90f9e8a6ca787af10cf99707a3c4c
Clement <56961627+Verboom30@users.noreply.github.com>
2026-05-08 01:51:26 +0200

    Ajout de toutes les fonctions

--- 01_SW/Robot_Main/lib/Package/main_pck.hpp
+++ 01_SW/Robot_Main/lib/Package/main_pck.hpp
@@ -14,46 +14,14 @@
 #define EN_SPREADCYCLE false // Toggle spreadCycle on TMC2208/2209/2224: default false, true: much faster!!!!
 #define PWM_AUTOSCALE true   // Needed for stealthChop
 
-struct pos_pince {
-    int pince_open;
-    int pince_close;
-    int pince_banner;
-};
-
-const struct pos_pince Pince[]{
-    {165, 140,130}, //Pince R1
-    {70, 60,0},  //Pince R2
-    {180, 160,0},   //Pince R3
-    {90, 70,50}, //Pince R4
-   
-};
-
-struct pos_bras {
-    int bras_home;
-    int bras_side;
-    int bras_take;
-    int bras_banner;
-    int bras_drop_banner;
-};
-
-const struct pos_bras Bras[]{
-    {8,40,147,180,140}, //Bras G
-    {185,160,40,10,40}, //Bras D
-    
-   
-};
-
-struct pos_hook {
-    int hook_up;
-    int hook_down;
-    int hook_take;
-};
-const struct pos_hook Hook[]{
-    {55,155,152}, //Hook G
-    {155,55,58}, //Hook D
-    
-   
-};
+#define pince1 0
+#define pince2 1
+#define pince3 2
+#define pince4 3
+#define pince_g 4
+#define pince_d 6
+#define pince_top 5
+#define bras 7
 
 enum {IDLE,START_UP,CAL,WAIT_MATCH,GAME,END}FsmState;  
 #endif // __MAIN_PKG_H_
--- 01_SW/Robot_Main/src/main.cpp
+++ 01_SW/Robot_Main/src/main.cpp
@@ -2,42 +2,186 @@
 #include "Stepper.hpp"
 #include "Holonome.hpp"
 #include "pinout.hpp"
-
+#include "ServoPCA9685.hpp"
+#include "main_pck.hpp"
+#include "ColorSensor.hpp"
+//***********************************/************************************
+//                                 MOVE                                 //
+//***********************************/************************************
 Stepper stepA(STEP_A,DIR_A);
 Stepper stepB(STEP_B,DIR_B);
 Stepper stepC(STEP_C,DIR_C);
 DigitalOut En_drive_N(EN_DRIVE_N);
+DigitalIn SW_Drive(SW_SPARE_1);
+Holonome robot(&stepA, &stepB, &stepC, &stopLidar);
+//***********************************/************************************
+//                                 SERVO                                //
+//***********************************/*************************************
+I2C i2c(SDA, SCL);
+ServoPCA9685 servoCard1(i2c, 0x40);
+ServoPCA9685 servoCard2(i2c, 0x41);
+ServoPCA9685 servoCard3(i2c, 0x42);
+//***********************************/************************************
+//                                 SENSOR                               //
+//***********************************/************************************
+TCS34007Mux sensor1(i2c, 0x70);
+TCS34007Mux sensor2(i2c, 0x71);
+TCS34007Mux sensor3(i2c, 0x72);
+TCS34007Mux* cards[3] = {
+    &sensor1,
+    &sensor2,
+    &sensor3
+};
+//***********************************/************************************
+//                                 LIDAR                               //
+//***********************************/************************************
 DigitalOut Led(LIDAR_LED);
 
-DigitalIn SW_Drive(SW_SPARE_1);
 bool stopLidar = false;
 
-Holonome robot(&stepA, &stepB, &stepC, &stopLidar);
+void  disable_all_mux(){
+    sensor1.disable();
+    sensor2.disable();
+    sensor3.disable();
+    ThisThread::sleep_for(2ms);
+}
+
+void initSensor()
+{
+    printf("START Sensor\r\n");
+    disable_all_mux();
+
+    sensor1.begin();
+    disable_all_mux();
+
+    sensor2.begin();
+    disable_all_mux();
+
+    sensor3.begin();
+    disable_all_mux();
+
+    sensor1.calibrateBaseline();
+    disable_all_mux();
+
+    sensor2.calibrateBaseline();
+    disable_all_mux();
+
+    sensor3.calibrateBaseline();
+    disable_all_mux();
+}
+
+void readsensor(){
+    for (uint8_t card = 0; card < 3; card++)
+    {
+        disable_all_mux();
+
+        for (uint8_t ch = 0; ch < 4; ch++)
+        {
+            TCS34007Mux::TcsColor c;
+            auto color = cards[card]->readAndDetect(ch, c);
+
+            printf("Carte %d canal %d : C=%u R=%u G=%u B=%u -> %s\r\n",
+                    card,
+                    ch,
+                    c.clear,
+                    c.red,
+                    c.green,
+                    c.blue,
+                    TCS34007Mux::colorToString(color));
+        }
+    }
+    disable_all_mux();
+}
+
+void configureCard(ServoPCA9685& card)
+{
+    card.setMirrored(0, false);
+    card.setMirrored(1, true);
+    card.setMirrored(2, false);
+    card.setMirrored(3, true);
+    card.setMirrored(4, false);
+    card.setMirrored(5, false);
+    card.setMirrored(6, true);
+    card.setMirrored(7, false);
+
+    card.setOffset(0, 0.0f);
+    card.setOffset(1, -5.0f);
+    card.setOffset(2, 0.0f);
+    card.setOffset(3, 0.0f);
+    card.setOffset(4, 0.0f);
+    card.setOffset(5, 0.0f);
+    card.setOffset(6, 0.0f);
+    card.setOffset(7, 0.0f);
+}
+
+void Home_Servo()
+{
+    servoCard2.setServoAngle(pince_top, 35);
+    servoCard2.setServoAngle(pince_g,   50); 
+    servoCard2.setServoAngle(pince_d,   50); 
+    servoCard2.setServoAngle(pince1,    90); 
+    servoCard2.setServoAngle(pince2,    90); 
+    servoCard2.setServoAngle(pince3,    90);
+    servoCard2.setServoAngle(pince4,    90);
+    ThisThread::sleep_for(500ms);
+    servoCard2.setServoAngle(bras, 18);
+}
+
+void Prise_Caise()
+{
+    servoCard2.setServoAngle(bras, 80);
+
+    servoCard2.setServoAngle(pince_top, 90);
+    servoCard2.setServoAngle(pince_g,   80); 
+    servoCard2.setServoAngle(pince_d,   80); 
+    servoCard2.setServoAngle(pince1,    60); 
+    servoCard2.setServoAngle(pince2,    60); 
+    servoCard2.setServoAngle(pince3,    60);
+    servoCard2.setServoAngle(pince4,    60);
+    ThisThread::sleep_for(500ms);
+    servoCard2.setServoAngle(bras, 140);
+    ThisThread::sleep_for(250ms);
+    servoCard2.setServoAngle(pince1,    90); 
+    servoCard2.setServoAngle(pince2,    90); 
+    servoCard2.setServoAngle(pince3,    90);
+    servoCard2.setServoAngle(pince4,    90);
+    ThisThread::sleep_for(500ms);
+   
+    servoCard2.setServoAngle(pince_g,   40); 
+    servoCard2.setServoAngle(pince_d,   40); 
+    ThisThread::sleep_for(250ms);
+    servoCard2.setServoAngle(pince_g,   50); 
+    servoCard2.setServoAngle(pince_d,   50); 
+    ThisThread::sleep_for(250ms);
+    servoCard2.setServoAngle(bras, 150);
+    servoCard2.setServoAngle(pince_top, 20);
+    ThisThread::sleep_for(250ms);
+    servoCard2.setServoAngle(pince_g,   35); 
+    servoCard2.setServoAngle(pince_d,   35); 
+    ThisThread::sleep_for(250ms);
+    servoCard2.setServoAngle(bras, 18);
+}
+
 
 int main()
 {
     En_drive_N = SW_Drive;
-    Led=! SW_Drive;
     SW_Drive.mode(PullUp);
-    //robot.run();
 
-    robot.setPosition(0, 0, 0);
-
-    // déplacement holonome direct :
+    i2c.frequency(100000);
+    initSensor();
+    printf("\r\nTest 3 cartes PCA9685\r\n");
+    configureCard(servoCard1);
+    configureCard(servoCard2);
+    configureCard(servoCard3);
+    servoCard1.begin();
+    servoCard2.begin();
+    servoCard3.begin();
+    Home_Servo();
     
-     robot.Robotmove(0, 1000, 0);      // +Y
-     
-    // robot.Robotmove(0, 0, 90);       // rotation
-
-    // // aller à une position :
-    // robot.Robotgoto(1000, 500, 0);
+    
 
     while (true) {
-        printf("X=%.1f Y=%.1f T=%.1f\r\n",
-               robot.getPositionX(),
-               robot.getPositionY(),
-               robot.getTheta());
-
-        ThisThread::sleep_for(500ms);
+    
     }
 }
